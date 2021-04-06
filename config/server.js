de7da7cd6d1e499a9faa53cf386e4f37f9514db1
Shubham Jha <shubham.jha2001m@gmail.com>
2021-04-06 18:44:30 +0530

    Hard Reset

--- config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '21bbfbcfa86140ed35380e49e32b5a19'),
-    },
-  },
-});
--- extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'fcc7fc14-1860-45ad-b991-be472f48ff98'
-};

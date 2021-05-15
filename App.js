b81fbfadbf18f3b6b64fcb31d5a4cac4f9803a2a
Shubham Jha <shubham.jha2001m@gmail.com>
2021-05-15 23:07:33 +0530

    Custom Fonts

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import Home from './screens/Home';
 
+const getFonts = () => {
+    return Font.loadAsync({
+        'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+        'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf')
+    });
+}
+
 export default function App() {
-  return (
-    <>
-      <Home />
-      <StatusBar style="auto" />
-    </>
-  );
+
+    const [fontsLoaded, setFontsLoaded] = useState(false);
+
+    if(fontsLoaded) {
+        return (
+            <Home />
+        );
+    }
+
+    else {
+        return (
+            <AppLoading
+                startAsync={getFonts}
+                onFinish={() => setFontsLoaded(true)}
+                onError={console.warn}
+            />
+        );
+    }
+
 }

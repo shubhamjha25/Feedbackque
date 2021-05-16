72a9543354e3bbcb4e4a411e87485f7b7456a54b
Shubham Jha <shubham.jha2001m@gmail.com>
2021-05-16 18:07:13 +0530

    Passing Data Between Screens

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 const Home = (props) => {
 
-    const pressHandler = () => props.navigation.push('ReviewDetails');
+    const [reviews, setReviews] = useState([
+        { title: 'Zelda, Breathe of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' }, 
+        { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorepum ipsum tara rum pum pum', key: '2' },
+        { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum color de met', key: '3' }
+    ]);
 
     return (
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>Home Screen</Text>
-            <Button title='Go To Review Details' onPress={pressHandler} />
+            <FlatList 
+                data={reviews}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => props.navigation.push('ReviewDetails', item)}>
+                        <Text style={globalStyles.titleText}>{item.title}</Text>
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     );
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 const ReviewDetails = (props) => {
 
-    const pressHandler = () => props.navigation.goBack();
-
     return (
         <View style={globalStyles.container}>
-            <Text>Review Details Screen</Text>
-            <Button title='Back To Home Screen' onPress={pressHandler} />
+            <Text>{ props.navigation.getParam('title') }</Text>
+            <Text>{ props.navigation.getParam('body') }</Text>
+            <Text>{ props.navigation.getParam('rating') }</Text>
         </View>
     );
 }

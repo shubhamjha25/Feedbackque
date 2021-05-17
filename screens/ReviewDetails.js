faf0bfe6ec492a6b4fa8ae60f1fbebd00a2129bd
Shubham Jha <shubham.jha2001m@gmail.com>
2021-05-17 19:42:13 +0530

    Images

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import Card from '../shared/Card';
-import { globalStyles } from '../styles/global';
+import { globalStyles, images } from '../styles/global';
 
 const ReviewDetails = (props) => {
 
@@ -10,10 +10,24 @@ const ReviewDetails = (props) => {
             <Card>
                 <Text>{ props.navigation.getParam('title') }</Text>
                 <Text>{ props.navigation.getParam('body') }</Text>
-                <Text>{ props.navigation.getParam('rating') }</Text>
+                <View style={styles.rating}>
+                     <Text>GameZone Rating : </Text>
+                     <Image source={images.ratings[props.navigation.getParam('rating')]} />
+                </View>
             </Card>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    rating: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        paddingTop: 16,
+        marginTop: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#eee',
+    }
+});
 
 export default ReviewDetails;

93f9cc778cd15011d5b232afedd5bbbd1a88a7c6
Shubham Jha <shubham.jha2001m@gmail.com>
2021-05-17 20:01:16 +0530

    Background Images

--- shared/Header.js
+++ shared/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image, ImageBackground } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const Header = (props) => {
@@ -9,12 +9,13 @@ const Header = (props) => {
     }
 
     return (
-        <View style={styles.header}>
+        <ImageBackground source={require('../assets/game_bg.png')} style={styles.header}>
             <MaterialIcons name='menu' size={28} onPress={openMenu} style={styles.icon} />
-            <View>
-                <Text style={styles.headerText}>{ props.title }</Text>
+            <View style={styles.headerTitle}>
+                <Image source={require('../assets/heart_logo.png')} style={styles.headerImage} />
+                <Text style={styles.headerText}>{props.title}</Text>
             </View>
-        </View>
+        </ImageBackground>
     );
 }
 
@@ -24,18 +25,26 @@ const styles = StyleSheet.create({
         height: '100%',
         flexDirection: 'row',
         alignItems: 'center',
-        justifyContent: 'center'   
+        justifyContent: 'center',
     },
     headerText: {
         fontWeight: 'bold',
         fontSize: 20,
         color: '#333',
-        letterSpacing: 1
+        letterSpacing: 1,
     },
     icon: {
         position: 'absolute',
-        left: 16
-    }
+        left: 16,
+    },
+    headerTitle: {
+        flexDirection: 'row'
+    },
+    headerImage: {
+        width: 26,
+        height: 26,
+        marginHorizontal: 10
+    },
 });
 
 export default Header;
